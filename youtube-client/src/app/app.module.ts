33cd063af5c8708867da0f13ea2026a6eeb9b265
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-10-11 21:42:34 +0300

    feat: add Angular Material

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { SearchItemComponent } from './components/search-item/search-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
