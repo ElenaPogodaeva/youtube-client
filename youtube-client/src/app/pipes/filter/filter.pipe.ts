575ccdd4a6444f2af4af6550194c612afce5dafc
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-10-12 21:24:37 +0300

    feat: add filter pipe

---
+++ youtube-client/src/app/pipes/filter/filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { SearchItemModel } from '../../models/search-item.model';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: SearchItemModel[], searchTerm: string): SearchItemModel[] {
+    if (!value || !value.length || !searchTerm) return value;
+
+    return value.filter((item) =>
+      item.snippet.title.toLowerCase().includes(searchTerm.toString().toLowerCase()),
+    );
+  }
+}
