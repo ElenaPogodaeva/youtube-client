f375663497fe0beb4d0ebb769f5b51c97538fa56
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-10-22 19:19:24 +0300

    feat: add not-found page

--- youtube-client/src/app/app-routing.module.ts
+++ youtube-client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import type { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   {
@@ -19,10 +20,10 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
-  // {
-  //   path: '**',
-  //   component: NotFoundPageComponent,
-  // },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
 ];
 
 @NgModule({
--- youtube-client/src/app/core/core.module.ts
+++ youtube-client/src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NotFoundPageComponent],
   imports: [CommonModule, SharedModule, FormsModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, NotFoundPageComponent],
 })
 export class CoreModule {}
