649db523a75154b3a3a0cf2210618670cd5e76de
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-11-10 00:46:40 +0300

    feat: add login form validation

--- youtube-client/src/app/auth/components/login-form/login-form.component.ts
+++ youtube-client/src/app/auth/components/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import type { OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { ValidationService } from '../../../../app/core/services/validation.service';
 
 @Component({
   selector: 'app-login-form',
@@ -13,17 +14,30 @@ export class LoginFormComponent implements OnInit {
 
   token: string = 'some-token';
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  hide = true;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private validationService: ValidationService,
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       login: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required, this.validationService.passwordValidator()]],
     });
+  }
+
+  get login() {
+    return this.loginForm.get('login');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
   public submit() {
     this.authService.login(this.token);
-    console.log(this.loginForm.value);
   }
 }
